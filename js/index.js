4a59b0c5e8e6eaa48bcf256f1c63e725b961bbc3
themikail <hello@mikailgokce.com>
2023-03-28 14:54:20 +0200

    re push

--- js/index.js
+++ js/index.js
@@ -6,16 +6,30 @@ console.clear();
 
 // 1. Store the sentence 'Happy coding!' in a variable named 'coding'.
 
+var coding = "Happy coding!";
+
 // 2. Display the variable 'coding' as a message in the console.
+
+console.log("Have", coding);
 
 // 3. Store the sentence 'Happy hacking!' in the variable named 'hacking'.
 
+var hacking = "Happy hacking!";
+
 // 4. Display the variable 'hacking' as a message in the console.
+
+console.log(hacking, "/cw Kali linux");
 
 // 5. Which method is used to clear the console? Use it to tidy up the console.
 
+console.clear();
+
 // 6. Store the text 'This is an error!' in the variable 'error'.
 
+const error = "This is an error!";
+
 // 7. Which method is used to display an error message in the console? Use it do display the error message in the console.
+
+console.error(error);
 
 // Well done!
